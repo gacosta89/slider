32344bf53798931e16d64d61b4929c90d2f2d573
Gonzalo Acosta <gacosta@dataart.com>
2017-04-27 18:09:29 -0300

    use json and http server

---
+++ README.md
@@ -0,0 +1,21 @@
+## HTML
+
+### Elementos
+
+html, head, body, title, link, script, img, div, i
+
+### Atributos
+
+id, class, src (img)
+
+## CSS
+
+selectores, width, height, flexbox, fonts
+
+## JAVASCRIPT
+
+jquery, function, var, slick slider
+
+## JAVASCRIPT *
+
+json, ajax, map, filter
--- static/index.js
+++ static/index.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
-    var container = $('.images-container')
+    var container = $('.images-container');
 
-    container.slick({
-        dots: false,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 1,
-        arrows: false,
-        infinite: true,
-    });
+    $.getJSON('static/data.json', function (data) {
+        data.autos.forEach((auto) => {
+            container.append(`
+                <div class="image">
+                    <img src="static/images/${auto.foto}" />
+                </div>
+            `);
+        });
 
-    $('#prev').on('click', function () {
-        container.slick('slickPrev');
-    });
+        setTimeout(() => {
+            container.slick({
+                dots: false,
+                infinite: false,
+                speed: 300,
+                slidesToShow: 1,
+                arrows: false,
+                infinite: true,
+            });
 
-    $('#next').on('click', function () {
-        container.slick('slickNext');
+            $('#prev').on('click', function () {
+                container.slick('slickPrev');
+            });
+
+            $('#next').on('click', function () {
+                container.slick('slickNext');
+            });
+        }, 1000);
     });
 });
